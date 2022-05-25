16dc87f52e37db748b86a28a51343c410c2ffb56
Qiu-Weidong <568385476@qq.com>
2022-05-25 20:14:01 +0800

    .

--- src/views/VideoDetail.vue
+++ src/views/VideoDetail.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <el-row>
-    <el-button @click="multi">下载视频</el-button>
-    </el-row>
-
-    <el-row>
+    <el-dialog v-model="dialogVisible" width="70%">
       <video id="mse" autoplay="true" playsinline controls="controls" style="width:100%">
       <source
         src="../assets/img/video.mp4"
@@ -12,6 +8,22 @@
       />
       你的浏览器不支持Video标签
     </video>
+    </el-dialog>
+    <el-row>
+      <el-button @click="multi">下载视频</el-button>
+    </el-row>
+
+    <el-row>
+      <video
+        id="mse"
+        autoplay="true"
+        playsinline
+        controls="controls"
+        style="width: 100%"
+      >
+        <source src="../assets/img/video.mp4" type="video/mp4" />
+        你的浏览器不支持Video标签
+      </video>
     </el-row>
 
     <!-- <el-row v-if="multi" >
@@ -21,13 +33,21 @@
 
     <el-row>
       <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="date" label="日期" width="240">
+        <el-table-column prop="date" label="日期" width="180">
         </el-table-column>
-        <el-table-column prop="id" label="物体id" width="240">
+        <el-table-column prop="time" label="时间" width="180">
         </el-table-column>
-        <el-table-column prop="address1" label="移动前坐标" width="240">
+        <el-table-column prop="id" label="物体id" width="180">
         </el-table-column>
-        <el-table-column prop="address2" label="移动后坐标"> </el-table-column>
+        <el-table-column prop="address1" label="移动前坐标" width="180">
+        </el-table-column>
+        <el-table-column prop="address2" label="移动后坐标" width="180">
+        </el-table-column>
+        <el-table-column fixed="right" label="操作">
+          <el-button type="text" size="small" @click="show">
+            查看发生移动的那段视频
+          </el-button>
+        </el-table-column>
       </el-table>
     </el-row>
     <!-- <el-row>
@@ -37,38 +57,52 @@
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "VideoDetail",
   setup() {
+    const dialogVisible = ref(false);
     return {
+      dialogVisible,
       url: "https://cdn.jsdelivr.net/gh/Qiu-Weidong/pictures/images/毕设/微信图片_20220504162039.jpg",
       tableData: [
         {
           date: "2022-5-10",
+          time: "16:35:17",
           id: "椅子_1",
           address1: "(123,456,789)",
           address2: "(123, 123,123)",
         },
         {
-          date: "2022-4-17",
+          date: "2022-5-10",
+          time: "16:23:17",
           id: "广告牌_2",
           address1: "(12,13,14)",
           address2: "(17, 23, 90)",
         },
         {
-          date: "2022-3-12",
+          date: "2022-5-10",
+          time: "16:35:23",
           id: "椅子_1",
           address1: "(14, 231, 89)",
           address2: "(132,32,56)",
         },
         {
-          date: "2022-1-1",
+          date: "2022-5-10",
+          time: "16:32:17",
           id: "椅子_4",
           address1: "(14, 98, 45)",
           address2: "(92, 91, 78)",
         },
       ],
     };
+  },
+  methods: {
+    show() {
+      console.log("查看发生移动的那段视频");
+      this.dialogVisible = true;
+    },
   },
 };
 </script>
